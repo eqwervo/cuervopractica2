85953fcfc8b70306725011670196aa481e6dd742
esteban cuervo <cuervoesteban87@gmail.comm>
2025-03-23 23:25:21 -0300

    cambio en estructuras de almacenamiento y recuento de puntaje

--- src/practica2_10/main.py
+++ src/practica2_10/main.py
@@ -1,18 +1,12 @@
 import roundsdb
 
-players = []
-
+# Marcadores iniciales
+players = {}
 for round in roundsdb.rounds:
     for name in round:
-        players.append(name)
+        players[name] = {'kills': 0, 'assists': 0, 'deaths': 0, 'mvp': 0, 'points': 0}
+
         
-# Marcadores iniciales
-players_points = {'Shadow': 0, 'Blaze': 0, 'Viper': 0, 'Frost': 0, 'Reaper': 0} 
-players_mvp = {'Shadow': 0, 'Blaze': 0, 'Viper': 0, 'Frost': 0, 'Reaper': 0}
-player_deaths = {'Shadow': 0, 'Blaze': 0, 'Viper': 0, 'Frost': 0, 'Reaper': 0}
-player_kills = {'Shadow': 0, 'Blaze': 0, 'Viper': 0, 'Frost': 0, 'Reaper': 0}
-player_assists = {'Shadow': 0, 'Blaze': 0, 'Viper': 0, 'Frost': 0, 'Reaper': 0}
-    
 
 
 
@@ -20,44 +14,26 @@ player_assists = {'Shadow': 0, 'Blaze': 0, 'Viper': 0, 'Frost': 0, 'Reaper': 0}
 # almacenando key y value que finalmente permite accede a los valores 
 # y hacer las operaciones
 for i,round in enumerate(roundsdb.rounds):
+    score_max = 0
+    round_score = int(0)
+    mvp = None
     round = roundsdb.rounds[i]
     for player, value in round.items():
-        players_points[player] += value['kills'] * 3
-        player_kills[player] += value['kills']
-        players_points[player] += value['assists']
-        player_assists[player] += value['assists']
+        players[player]['kills'] += value['kills']
+        players[player]['assists'] += value['assists']
+        round_score = value['kills'] * 3 + value['assists']
         if value['deaths'] == True:
-            players_points[player] -= 1
-            player_deaths[player] += 1
-    #print(f'Marcador al finalizar ronda {i+1} \n{players_points}')
-    mvp = max(players_points, key=players_points.get)
-    players_mvp[mvp] += 1
-    print(f'''
-    Jugador     Kills       Assists     Deaths      MVPs        Totales    
-    {players[0]} {player_kills[players[0]]:>8}{player_assists[players[0]]:>13}{player_deaths[players[0]]:>11}{players_mvp[players[0]]:>11}{players_points[players[0]]:>14}
-    {players[1]} {player_kills[players[1]]:>9}{player_assists[players[1]]:>13}{player_deaths[players[1]]:>11}{players_mvp[players[1]]:>11}{players_points[players[1]]:>14}   
-    {players[2]} {player_kills[players[2]]:>9}{player_assists[players[2]]:>13}{player_deaths[players[2]]:>11}{players_mvp[players[2]]:>11}{players_points[players[2]]:>14}   
-    {players[3]} {player_kills[players[3]]:>9}{player_assists[players[3]]:>13}{player_deaths[players[3]]:>11}{players_mvp[players[3]]:>11}{players_points[players[3]]:>14}
-    {players[4]} {player_kills[players[4]]:>8}{player_assists[players[4]]:>13}{player_deaths[players[4]]:>11}{players_mvp[players[4]]:>11}{players_points[players[4]]:>14}  
-    ''')
-
-    
-    
-print(f'MVP ronda {i+1}: {mvp}')
-print()
-
-'''
-print('Randking final')
-print(f
-Jugador     Kills       Assists     Deaths      MVPs        Totales    
-{players[0]} {player_kills[players[0]]:>8}{player_assists[players[0]]:>13}{players_mvp[players[0]]:>8}{player_kills[players[0]]:>8}{players_points[players[0]]:>8}
-{players[1]}    
-{players[2]}    
-{players[3]}
-{players[4]}    
-    '''
-
-    
-            
-
-        
+            players[player]['deaths'] += 1
+            round_score -= 1      
+        players[player]['points'] += round_score
+        if round_score > score_max:
+            score_max = round_score
+            mvp = player
+    players[mvp]['mvp'] += 1
+    print(f'Marcador al finalizar ronda {i+1}')
+    print(f'MVP de la ronda: {mvp}')
+    final_score = sorted(players.items(), key=lambda x: x[1]['points'], reverse=True)
+    print(f'''Jugador     Kills       Assists     Deaths      MVPs        Totales''')
+    for player, stats in final_score:
+        print(f'''{player:8} {stats['kills']:>6}{stats['assists']:>13}{stats['deaths']:>12}{stats['mvp']:>11}{stats['points']:>13}''')
+    print() 
