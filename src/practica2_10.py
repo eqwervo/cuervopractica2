adb11060458494caa3dba446a664e8165c3b6bfe
esteban cuervo <cuervoesteban87@gmail.comm>
2025-03-27 21:04:19 -0300

    agregué documentación y mejore impresiones de pantalla

--- src/practica2_10.py
+++ src/practica2_10.py
@@ -17,6 +17,7 @@ def solution():
         round_score = int(0)
         mvp = None
         round = rounds[i]
+        # Recorre por clave valor los items de round y asigna puntajes y estadisticas
         for player, value in round.items():
             players[player]['kills'] += value['kills']
             players[player]['assists'] += value['assists']
@@ -25,15 +26,18 @@ def solution():
                 players[player]['deaths'] += 1
                 round_score -= 1      
             players[player]['points'] += round_score
+            # Evalua si es MVP de la ronda
             if round_score > score_max:
                 score_max = round_score
                 mvp = player
         #finalizada la ronda se registra el mvp, se ordena la informacion y se imprime el score
         players[mvp]['mvp'] += 1
-        print(f'Marcador al finalizar ronda {i+1}')
-        print(f'MVP de la ronda: {mvp}')
+        print((f'Marcador al finalizar ronda {i+1}').center(60))
+        print(f'MVP de la ronda: {mvp}'.center(60))
+        print()
         final_score = sorted(players.items(), key=lambda x: x[1]['points'], reverse=True)
         print(f'''Jugador     Kills       Assists     Deaths      MVPs        Totales''')
         for player, stats in final_score:
             print(f'''{player:8} {stats['kills']:>6}{stats['assists']:>13}{stats['deaths']:>12}{stats['mvp']:>11}{stats['points']:>13}''')
         print() 
+solution()
